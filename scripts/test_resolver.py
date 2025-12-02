b0fe72dcb4ba472dcfedea04834dcaa38cdd6f84
meltedtuna <meltedtuna@yahoo.com>
2025-12-02 19:39:02 -0300

    Delete scripts directory

--- scripts/test_resolver.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python3
-import subprocess, sys
-print("Running tag resolver unit tests (pytest)...")
-rc = subprocess.call([sys.executable, "-m", "pytest", "tests/test_tag_resolver.py", "-q"])
-sys.exit(rc)
