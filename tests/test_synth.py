ee2c4833177d61b34d186e116045d4f0d9598445
meltedtuna <meltedtuna@yahoo.com>
2025-12-05 18:16:52 -0300

    Delete tests directory

--- tests/test_synth.py
+++
@@ -1,5 +0,0 @@
-from src.tools.generate_synthetic import generate_sequence
-def test_generate_shape():
-    data = generate_sequence(duration_s=1)
-    assert data.ndim == 2
-    assert data.shape[1] >= 8
